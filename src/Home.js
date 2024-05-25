50843bfde1082dac5c1cefeb50400660dc79b389
lisadeshishku <119889136+lisadeshishku@users.noreply.github.com>
2024-05-25 13:25:06 +0200

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+// src/components/MyComponent.js
+
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1> Zenit School!</h1>
+    </div>
+  );
+};
+
+export default MyComponent;
