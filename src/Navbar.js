97e8694259b7e739a0b33b92b3223642e5293cea
Lisa Deshishku <119889136+lisadeshishku@users.noreply.github.com>
2024-05-26 20:35:27 +0200

    webpack issues resolved

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ import './navbar.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="./Home.js" className="home-link">
+      <Link to="/" className="home-link">
         Home
       </Link>
     </nav>
